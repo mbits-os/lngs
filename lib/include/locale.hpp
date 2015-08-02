4816bbc9c4a0b2968888527453dc249dff3b9bae
Marcin Zdun <mzdun@midnightbits.com>
2015-08-02 21:05:41 +0200

    [LIBSTR-4] Top-level classes for the `lngs enum'

---
+++ lib/include/locale.hpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2015 midnightBITS
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <locale_storage.hpp>
+#include <format.hpp>
+
+namespace locale {
+	template <typename Enum, typename Storage = storage::FileBased>
+	class SingularStrings : public Storage {
+	public:
+		std::string operator()(Enum val) const noexcept
+		{
+			auto ptr = Storage::get_string((uint32_t)val);
+			return ptr ? ptr : std::string{ };
+		}
+
+		template <typename... Args>
+		std::string operator()(Enum val, Args&&... args) const noexcept
+		{
+			auto ptr = Storage::get_string((uint32_t)val);
+			return ptr ? ::str::format(ptr, std::forward<Args>(args)...) : std::string{ };
+		}
+
+		std::string attr(v1_0::attr_t val) const noexcept
+		{
+			auto ptr = Storage::get_attr(val);
+			return ptr ? ptr : std::string{ };
+		}
+	};
+
+	template <typename Enum, typename Storage = storage::FileBased>
+	class PluralOnlyStrings : public Storage {
+	public:
+		std::string operator()(Enum val, intmax_t count) const noexcept
+		{
+			auto ptr = Storage::get_string((uint32_t)val);
+			return ptr ? ptr : std::string{ };
+		}
+
+		template <typename... Args>
+		std::string operator()(Enum val, intmax_t count, Args&&... args) const noexcept
+		{
+			auto ptr = Storage::get_string((uint32_t)val);
+			return ptr ? ::str::format(ptr, std::forward<Args>(args)...) : std::string{ };
+		}
+	};
+
+	template <typename SEnum, typename PEnum, typename Storage = storage::FileBased>
+	class StringsWithPlurals : public SingularStrings<SEnum, Storage> {
+	public:
+		using SingularStrings<SEnum, Storage>::operator(); // un-hide
+		std::string operator()(PEnum val, intmax_t count) const noexcept
+		{
+			auto ptr = SingularStrings<SEnum, Storage>::get_string((uint32_t)val, count);
+			return ptr ? ptr : std::string{ };
+		}
+
+		template <typename... Args>
+		std::string operator()(PEnum val, intmax_t count, Args&&... args) const noexcept
+		{
+			auto ptr = SingularStrings<SEnum, Storage>::get_string((uint32_t)val, count);
+			return ptr ? ::str::format(ptr, std::forward<Args>(args)...) : std::string{ };
+		}
+	};
+}
---
+++ lib/include/locale_storage.hpp
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2015 midnightBITS
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <translation.hpp>
+#include <assert.h>
+
+namespace locale {
+	namespace storage {
+		class FileBased {
+			std::shared_ptr<translation> m_impl;
+
+		protected:
+			const char* get_string(uint32_t val) const noexcept
+			{
+				assert(m_impl);
+				return m_impl->get_string(val);
+			}
+
+			const char* get_string(uint32_t val, intmax_t count) const noexcept
+			{
+				assert(m_impl);
+				return m_impl->get_string(count, val);
+			}
+
+			const char* get_attr(uint32_t val) const noexcept
+			{
+				assert(m_impl);
+				return m_impl->get_attr(val);
+			}
+
+			const char* get_key(uint32_t val) const noexcept
+			{
+				assert(m_impl);
+				return m_impl->get_key(val);
+			}
+
+			uint32_t find_key(const char* val) const noexcept
+			{
+				assert(m_impl);
+				return m_impl->find_key(val);
+			}
+
+
+		public:
+			template <typename Manager, typename... Args>
+			void path_manager(Args&&... args)
+			{
+				m_impl = std::make_shared<translation>();
+				m_impl->path_manager(Manager { std::forward<Args>(args)... });
+			}
+
+			bool open(const std::string& lng)
+			{
+				assert(m_impl);
+				return m_impl->open(lng);
+			}
+		};
+
+		template <typename ResourceT>
+		class Builtin {
+			std::shared_ptr<lang_file> m_file;
+		protected:
+			const char* get_string(uint32_t val) const noexcept
+			{
+				assert(m_file);
+				return m_file->get_string(val);
+			}
+
+			const char* get_string(uint32_t val, intmax_t count) const noexcept
+			{
+				assert(m_file);
+				return m_file->get_string(count, val);
+			}
+
+			const char* get_attr(uint32_t val) const noexcept
+			{
+				assert(m_file);
+				return m_file->get_attr(val);
+			}
+
+			const char* get_key(uint32_t val) const noexcept
+			{
+				assert(m_file);
+				return m_file->get_key(val);
+			}
+
+			uint32_t find_key(const char* val) const noexcept
+			{
+				assert(m_file);
+				return m_file->find_key(val);
+			}
+
+		public:
+			bool init()
+			{
+				m_file = std::make_shared<lang_file>();
+				memory_view view;
+				view.contents = ResourceT::data();
+				view.size = ResourceT::size();
+				return m_file->open(view);
+			}
+		};
+
+		template <typename ResourceT>
+		class FileWithBuiltin : private FileBased, private Builtin<ResourceT> {
+			using B1 = FileBased;
+			using B2 = Builtin<ResourceT>;
+		protected:
+			const char* get_string(uint32_t val) const noexcept
+			{
+				auto ret = B1::get_string(val);
+				if (ret)
+					return ret;
+				return B2::get_string(val);
+			}
+
+			const char* get_string(uint32_t val, intmax_t count) const noexcept
+			{
+				auto ret = B1::get_string(val, count);
+				if (ret)
+					return ret;
+				return B2::get_string(val, count);
+			}
+
+			const char* get_attr(uint32_t val) const noexcept
+			{
+				auto ret = B1::get_attr(val);
+				if (ret)
+					return ret;
+				return B2::get_attr(val);
+			}
+
+			const char* get_key(uint32_t val) const noexcept
+			{
+				auto ret = B1::get_key(val);
+				if (ret)
+					return ret;
+				return B2::get_key(val);
+			}
+
+			uint32_t find_key(const char* val) const noexcept
+			{
+				auto ret = B1::find_key(val);
+				if (ret != (uint32_t)-1)
+					return ret;
+				return B2::find_key(val);
+			}
+		public:
+			using FileBased::path_manager;
+			using FileBased::open;
+			using Builtin<ResourceT>::init;
+		};
+	}
+}
