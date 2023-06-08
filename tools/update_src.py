7dec06257119edd880e31c1c38de59971be03f56
Marcin Zdun <mzdun@midnightbits.com>
2023-06-08 11:42:04 +0200

    build: switch from tuple to typing.Tuple

--- tools/update_src.py
+++ tools/update_src.py
@@ -5,13 +5,22 @@
 import argparse
 import os
 import shutil
-from typing import Union
+from typing import Tuple, Union
 
-parser = argparse.ArgumentParser(description="Update source files based on files generated in binary dir")
-parser.add_argument("-S", metavar="<dir>", required=True, help="set the source directory")
-parser.add_argument("-B", metavar="<dir>", required=True, help="set the binary directory")
-parser.add_argument('source', nargs='+', metavar="<source>", help="set the file to be updated")
+parser = argparse.ArgumentParser(
+    description="Update source files based on files generated in binary dir"
+)
+parser.add_argument(
+    "-S", metavar="<dir>", required=True, help="set the source directory"
+)
+parser.add_argument(
+    "-B", metavar="<dir>", required=True, help="set the binary directory"
+)
+parser.add_argument(
+    "source", nargs="+", metavar="<source>", help="set the file to be updated"
+)
 args = parser.parse_args()
+
 
 def exists(dir_name: str, file_name: str) -> Union[float, None]:
     try:
@@ -19,13 +28,19 @@ def exists(dir_name: str, file_name: str) -> Union[float, None]:
     except FileNotFoundError:
         return None
 
-def split_or_dup(input:str) -> tuple[str, str]:
-    if ':' in input:
-        b, s = input.split(':', 1)
+
+def split_or_dup(input: str) -> Tuple[str, str]:
+    if ":" in input:
+        b, s = input.split(":", 1)
         return (b, s)
     return (input, input)
 
-sources = [split_or_dup(source) for source in args.source if exists(args.B, split_or_dup(source)[0])]
+
+sources = [
+    split_or_dup(source)
+    for source in args.source
+    if exists(args.B, split_or_dup(source)[0])
+]
 for bin_filename, src_filename in sources:
     copy = not os.path.exists(os.path.join(args.S, src_filename))
     if not copy:
@@ -37,4 +52,6 @@ for bin_filename, src_filename in sources:
 
     if copy:
         print("Updating {}".format(src_filename))
-        shutil.copy(os.path.join(args.B, bin_filename), os.path.join(args.S, src_filename))
+        shutil.copy(
+            os.path.join(args.B, bin_filename), os.path.join(args.S, src_filename)
+        )
